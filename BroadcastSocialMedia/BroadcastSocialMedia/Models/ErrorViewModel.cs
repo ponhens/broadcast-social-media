a44a62be33a52062d1ff866228d7fc2728faf9bd
ponhens <142414097+ponhens@users.noreply.github.com>
2024-02-19 16:51:31 +0100

    Create project

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BroadcastSocialMedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
