5da528c62b0ad62b3c3f405034f56a545db1e431
ponhens <142414097+ponhens@users.noreply.github.com>
2024-02-21 13:42:55 +0100

    Added search functionality

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using BroadcastSocialMedia.Data;
+using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BroadcastSocialMedia.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
+        {
+            if(viewModel.Search != null)
+            {
+                var users = await _dbContext.Users.Where(u => u.Name.Contains(viewModel.Search))
+                    .ToListAsync();
+                viewModel.Result = users;
+            }
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BroadcastSocialMedia/BroadcastSocialMedia/ViewModels/UsersIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BroadcastSocialMedia.Models;
+
+namespace BroadcastSocialMedia.ViewModels
+{
+    public class UsersIndexViewModel
+    {
+        public string Search { get; set; }
+
+        public List<ApplicationUser> Result { get; set; } = new List<ApplicationUser>();
+    }
+}
