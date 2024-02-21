678b13af042c6e1ce41ccf196c9d400ad69b00a2
ponhens <142414097+ponhens@users.noreply.github.com>
2024-02-21 19:38:40 +0100

    Added show Broadcasts

--- BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using BroadcastSocialMedia.Data;
+using BroadcastSocialMedia.Models;
 using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,10 +10,12 @@ namespace BroadcastSocialMedia.Controllers
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsersController(ApplicationDbContext dbContext)
+        public UsersController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
@@ -23,6 +27,23 @@ namespace BroadcastSocialMedia.Controllers
                 viewModel.Result = users;
             }
 
+
+            return View(viewModel);
+        }
+
+        [Route("/Users/{id}")]
+        public async Task<IActionResult> ShowUser(string id)
+        {
+            var broadCasts = await _dbContext.Broadcasts.Where(b => b.User.Id == id).OrderByDescending(b => b.Published).ToListAsync();
+            //Såhär skrev kursledaren// var user = await _userManager.GetUserAsync(User); // men jag tror att han gjorde fel så jag skrev som (se nedan) 
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+
+            var viewModel = new UsersShowUserViewModel()
+            {
+                Broadcasts = broadCasts,
+                User = user,
+            };
 
             return View(viewModel);
         }
---
+++ BroadcastSocialMedia/BroadcastSocialMedia/ViewModels/UsersShowUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BroadcastSocialMedia.Models;
+
+namespace BroadcastSocialMedia.ViewModels
+{
+    public class UsersShowUserViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public List<Broadcast> Broadcasts { get; set; }
+    }
+}
