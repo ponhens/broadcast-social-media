892c3072c63e320bde9f378fdacf1c6398ee6b06
ponhens <142414097+ponhens@users.noreply.github.com>
2024-03-08 12:29:43 +0100

    Making ProfileImageFilenameGUID nullable in Applicationuser

--- BroadcastSocialMedia/BroadcastSocialMedia/Models/ApplicationUser.cs
+++ BroadcastSocialMedia/BroadcastSocialMedia/Models/ApplicationUser.cs
@@ -7,6 +7,6 @@ namespace BroadcastSocialMedia.Models
         public string? Name { get; set; }
         public ICollection<Broadcast> Broadcasts { get; set; }
         public ICollection<ApplicationUser> ListeningTo { get; set; } = new List<ApplicationUser>();
-        public string ProfileImageFilenameGUID { get; set; }
+        public string ?ProfileImageFilenameGUID { get; set; }
     }
 }
