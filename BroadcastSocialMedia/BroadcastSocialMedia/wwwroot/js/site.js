9160afaee7b606ab7a3a2b1a05293c1355fd7762
ponhens <142414097+ponhens@users.noreply.github.com>
2024-03-19 13:55:15 +0100

    Adding Change Theme Function With JS

--- BroadcastSocialMedia/BroadcastSocialMedia/wwwroot/js/site.js
+++ BroadcastSocialMedia/BroadcastSocialMedia/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const switchThemeButton = document.getElementById('switchThemeButton');
+    const navbar = document.getElementById('navbar');
+    const navbarLinkContainer = document.getElementById('navbarLinksCointainer');
+/*    const body = document.body;*/
+    let currentTheme = 1
+    switchThemeButton.addEventListener('click', function () {
+
+
+        navbar.classList.remove('navbar_style' + currentTheme);
+        navbarLinkContainer.classList.remove('navbarLinksCointainer_style' + currentTheme);
+
+        currentTheme++;
+
+        if (currentTheme > 5) {
+            currentTheme = 1;
+        }
+
+        navbar.classList.add('navbar_style' + currentTheme);
+        navbarLinkContainer.classList.add('navbarLinksCointainer_style' + currentTheme);
+    });
+
+
+});
